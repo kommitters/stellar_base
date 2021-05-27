e38f4b23d921bb1887404123c401649ffe22efe2
Juan Hurtado <juanmahurtado10@gmail.com>
2021-05-27 15:17:40 -0500

    Library setup (#4)
    * Add package information
    
    * Add global library configuration
    
    * Add supervision tree

---
+++ config/test.exs
@@ -0,0 +1,3 @@
+use Mix.Config
+
+config :stellar, network: :test
--- mix.exs
+++ mix.exs
@@ -1,20 +1,29 @@
 defmodule Stellar.MixProject do
   use Mix.Project
 
+  @github_url "https://github.com/kommitters/stellar_sdk"
+  @version "0.0.1"
+
   def project do
     [
-      app: :stellar_sdk,
-      version: "0.1.0",
+      app: :stellar,
+      version: @version,
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "Stellar SDK",
+      description: description(),
+      source_url: @github_url,
+      package: package(),
+      docs: docs()
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {Stellar.Application, []}
     ]
   end
 
@@ -23,6 +32,31 @@ defmodule Stellar.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description, do: "Elixir SDK for the Stellar network"
+
+  defp package do
+    [
+      description: description(),
+      files: ["lib", "config", "mix.exs", "README*", "LICENSE"],
+      licenses: ["MIT"],
+      links: %{
+        Changelog: "#{@github_url}/blob/master/CHANGELOG.md",
+        GitHub: @github_url
+      }
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      name: "Stellar SDK",
+      source_ref: "v#{@version}",
+      source_url: @github_url,
+      canonical: "http://hexdocs.pm/stellar_sdk",
+      extras: ["README.md", "CHANGELOG.md"]
     ]
   end
 end
