106de0b6ad8274b278abe99a3695b7d44f363274
Juan Hurtado <juanmahurtado10@gmail.com>
2021-05-31 18:54:03 -0500

    HTTP client implementation tests (#7)
    * Add tests for the HTTP client implementation
    
    * Improve test assertions and description labels

--- mix.exs
+++ mix.exs
@@ -30,9 +30,8 @@ defmodule Stellar.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:hackney, "~> 1.17", optional: true}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:hackney, "~> 1.17", optional: true},
+      {:mox, "~> 1.0", only: :test}
     ]
   end
 
