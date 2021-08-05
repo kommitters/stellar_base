14305ec12240ac12fe122bfb212b58bf2e7d66dc
Juan Hurtado <juan@kommit.co>
2021-08-05 17:49:52 -0500

    Add String and Void types

---
+++ test/xdr/base/void_test.exs
@@ -0,0 +1,34 @@
+defmodule Stellar.XDR.VoidTest do
+  use ExUnit.Case
+
+  alias Stellar.XDR.Void
+
+  describe "Void" do
+    setup do
+      %{
+        void: Void.new(),
+        binary: ""
+      }
+    end
+
+    test "new/1" do
+      %Void{value: nil} = Void.new()
+    end
+
+    test "encode_xdr/1", %{void: void, binary: binary} do
+      {:ok, ^binary} = Void.encode_xdr(void)
+    end
+
+    test "encode_xdr!/1", %{void: void, binary: binary} do
+      ^binary = Void.encode_xdr!(void)
+    end
+
+    test "decode_xdr/2", %{void: void, binary: binary} do
+      {:ok, {^void, ""}} = Void.decode_xdr(binary)
+    end
+
+    test "decode_xdr!/2", %{void: void, binary: binary} do
+      {^void, ^binary} = Void.decode_xdr!(binary <> binary)
+    end
+  end
+end
