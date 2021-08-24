780f42f028a9729a789f6c05d19ee709d4a12647
Juan Hurtado <juan@kommit.co>
2021-08-24 18:35:08 -0500

    Add dialyzer dependency

--- mix.exs
+++ mix.exs
@@ -39,6 +39,7 @@ defmodule Stellar.MixProject do
       {:elixir_xdr, "~> 0.1.5"},
       {:ed25519, "~> 1.3"},
       {:crc, "~> 0.10.0"},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:excoveralls, "~> 0.14", only: :test, runtime: false},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.24", only: :dev, runtime: false}
