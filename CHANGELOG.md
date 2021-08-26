52bf6071dee0c9631e49439ba4adc583ff2bb785
Juan Hurtado <juanmahurtado10@gmail.com>
2021-08-26 11:57:46 -0500

    Bump library version to v0.1.1 (#47)
    * Bump library verstion to v.0.1.1
    
    * Update CI's Dialyzer integration

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.1.1 (26.08.2021)
+* Increase test coverage.
+* Integrate Dialyzer.
+
 ## 0.1.0 (09.08.2021)
 * Ed25519 KeyPair generation.
 * Transactions XDR types.
--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Stellar.MixProject do
   use Mix.Project
 
   @github_url "https://github.com/kommitters/stellar_base"
-  @version "0.1.0"
+  @version "0.1.1"
 
   def project do
     [
@@ -40,7 +40,7 @@ defmodule Stellar.MixProject do
       {:ed25519, "~> 1.3"},
       {:crc, "~> 0.10.0"},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
-      {:excoveralls, "~> 0.14", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.14", only: :test, runtime: false},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.24", only: :dev, runtime: false}
     ]
