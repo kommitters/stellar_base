bcddcfe82a70ab463bc78d326ab608b18ff0ce57
Juan Hurtado <juan@kommit.co>
2021-08-26 11:55:59 -0500

    Update CI's Dialyzer integration

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule Stellar.MixProject do
       {:ed25519, "~> 1.3"},
       {:crc, "~> 0.10.0"},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
-      {:excoveralls, "~> 0.14", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.14", only: :test, runtime: false},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.24", only: :dev, runtime: false}
     ]
