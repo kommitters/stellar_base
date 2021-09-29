d6062e260bba8de6e458dce1ce7362cd636be35b
Juan Hurtado <juan@kommit.co>
2021-09-29 15:38:31 -0500

    Add ClaimClaimableBalance operation

---
+++ lib/xdr/assets/claimable_balance_id.ex
@@ -0,0 +1,63 @@
+defmodule Stellar.XDR.ClaimableBalanceID do
+  @moduledoc """
+  Representation of Stellar `ClaimableBalanceID` type.
+  """
+  alias Stellar.XDR.{ClaimableBalanceIDType, Hash}
+
+  @behaviour XDR.Declaration
+
+  @arms [CLAIMABLE_BALANCE_ID_TYPE_V0: Hash]
+
+  @type claimable_balance_id :: Hash.t()
+
+  @type t :: %__MODULE__{claimable_balance_id: Hash.t(), type: ClaimableBalanceIDType.t()}
+
+  defstruct [:claimable_balance_id, :type]
+
+  @spec new(claimable_balance_id :: claimable_balance_id(), type :: ClaimableBalanceIDType.t()) ::
+          t()
+  def new(claimable_balance_id, %ClaimableBalanceIDType{} = type),
+    do: %__MODULE__{claimable_balance_id: claimable_balance_id, type: type}
+
+  @impl true
+  def encode_xdr(%__MODULE__{claimable_balance_id: claimable_balance_id, type: type}) do
+    type
+    |> XDR.Union.new(@arms, claimable_balance_id)
+    |> XDR.Union.encode_xdr()
+  end
+
+  @impl true
+  def encode_xdr!(%__MODULE__{claimable_balance_id: claimable_balance_id, type: type}) do
+    type
+    |> XDR.Union.new(@arms, claimable_balance_id)
+    |> XDR.Union.encode_xdr!()
+  end
+
+  @impl true
+  def decode_xdr(bytes, spec \\ union_spec())
+
+  def decode_xdr(bytes, spec) do
+    case XDR.Union.decode_xdr(bytes, spec) do
+      {:ok, {{type, claimable_balance_id}, rest}} ->
+        {:ok, {new(claimable_balance_id, type), rest}}
+
+      error ->
+        error
+    end
+  end
+
+  @impl true
+  def decode_xdr!(bytes, spec \\ union_spec())
+
+  def decode_xdr!(bytes, spec) do
+    {{type, claimable_balance_id}, rest} = XDR.Union.decode_xdr!(bytes, spec)
+    {new(claimable_balance_id, type), rest}
+  end
+
+  @spec union_spec() :: XDR.Union.t()
+  defp union_spec do
+    nil
+    |> ClaimableBalanceIDType.new()
+    |> XDR.Union.new(@arms)
+  end
+end
