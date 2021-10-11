ff2fca5f788aa0d15bc64be67b881814ce2a09f3
Juan Hurtado <juanmahurtado10@gmail.com>
2021-10-11 10:02:25 -0500

    Add XDR types for TransactionEnvelope and FeeBumpTransactionEnvelope (#76)

---
+++ lib/xdr/transactions/fee_bump_transaction_envelope.ex
@@ -0,0 +1,55 @@
+defmodule Stellar.XDR.FeeBumpTransactionEnvelope do
+  @moduledoc """
+  Representation of Stellar `FeeBumpTransactionEnvelope` type.
+  """
+  alias Stellar.XDR.{DecoratedSignatures, FeeBumpTransaction}
+
+  @behaviour XDR.Declaration
+
+  @struct_spec XDR.Struct.new(tx: FeeBumpTransaction, signatures: DecoratedSignatures)
+
+  @type t :: %__MODULE__{tx: FeeBumpTransaction.t(), signatures: DecoratedSignatures.t()}
+
+  defstruct [:tx, :signatures]
+
+  @spec new(tx :: FeeBumpTransaction.t(), signatures :: DecoratedSignatures.t()) :: t()
+  def new(%FeeBumpTransaction{} = tx, %DecoratedSignatures{} = signatures),
+    do: %__MODULE__{tx: tx, signatures: signatures}
+
+  @impl true
+  def encode_xdr(%__MODULE__{tx: tx, signatures: signatures}) do
+    [tx: tx, signatures: signatures]
+    |> XDR.Struct.new()
+    |> XDR.Struct.encode_xdr()
+  end
+
+  @impl true
+  def encode_xdr!(%__MODULE__{tx: tx, signatures: signatures}) do
+    [tx: tx, signatures: signatures]
+    |> XDR.Struct.new()
+    |> XDR.Struct.encode_xdr!()
+  end
+
+  @impl true
+  def decode_xdr(bytes, struct \\ @struct_spec)
+
+  def decode_xdr(bytes, struct) do
+    case XDR.Struct.decode_xdr(bytes, struct) do
+      {:ok, {%XDR.Struct{components: [tx: tx, signatures: signatures]}, rest}} ->
+        {:ok, {new(tx, signatures), rest}}
+
+      error ->
+        error
+    end
+  end
+
+  @impl true
+  def decode_xdr!(bytes, struct \\ @struct_spec)
+
+  def decode_xdr!(bytes, struct) do
+    {%XDR.Struct{components: [tx: tx, signatures: signatures]}, rest} =
+      XDR.Struct.decode_xdr!(bytes, struct)
+
+    {new(tx, signatures), rest}
+  end
+end
