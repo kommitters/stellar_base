7ff85c03dd80b12ff3452e68ec9c54ae0d4500b8
Juan Hurtado <juanmahurtado10@gmail.com>
2021-10-13 14:33:01 -0500

    Add XDR types for CreateAccountResult, PaymentResult and SimplePaymentResult (#82)

---
+++ test/xdr/operations/payment_result_test.exs
@@ -0,0 +1,53 @@
+defmodule Stellar.XDR.Operations.PaymentResultTest do
+  use ExUnit.Case
+
+  alias Stellar.XDR.Void
+  alias Stellar.XDR.Operations.{PaymentResult, PaymentResultCode}
+
+  describe "PaymentResult" do
+    setup do
+      code = PaymentResultCode.new(:PAYMENT_SUCCESS)
+
+      %{
+        code: code,
+        value: Void.new(),
+        result: PaymentResult.new(Void.new(), code),
+        binary: <<0, 0, 0, 0>>
+      }
+    end
+
+    test "new/1", %{code: code, value: value} do
+      %PaymentResult{code: ^code, result: ^value} = PaymentResult.new(value, code)
+    end
+
+    test "encode_xdr/1", %{result: result, binary: binary} do
+      {:ok, ^binary} = PaymentResult.encode_xdr(result)
+    end
+
+    test "encode_xdr!/1", %{result: result, binary: binary} do
+      ^binary = PaymentResult.encode_xdr!(result)
+    end
+
+    test "encode_xdr!/1 with a default value", %{code: code, binary: binary} do
+      result = PaymentResult.new("TEST", code)
+      ^binary = PaymentResult.encode_xdr!(result)
+    end
+
+    test "decode_xdr/2", %{result: result, binary: binary} do
+      {:ok, {^result, ""}} = PaymentResult.decode_xdr(binary)
+    end
+
+    test "decode_xdr!/2", %{result: result, binary: binary} do
+      {^result, ^binary} = PaymentResult.decode_xdr!(binary <> binary)
+    end
+
+    test "decode_xdr!/2 an error code" do
+      {%PaymentResult{code: %PaymentResultCode{identifier: :PAYMENT_UNDERFUNDED}}, ""} =
+        PaymentResult.decode_xdr!(<<255, 255, 255, 254>>)
+    end
+
+    test "decode_xdr/2 with an invalid binary" do
+      {:error, :not_binary} = PaymentResult.decode_xdr(123)
+    end
+  end
+end
