8ad10fd8be44347e42b97f2d0d5dd3ece40560bb
Juan Hurtado <juan@kommit.co>
2021-10-14 14:59:16 -0500

    XDR types for AccountMergeResult, ManageDataResult and BumpSequenceResult

---
+++ lib/xdr/operations/account_merge_result.ex
@@ -0,0 +1,60 @@
+defmodule Stellar.XDR.Operations.AccountMergeResult do
+  @moduledoc """
+  Representation of Stellar `AccountMergeResult` type.
+  """
+  alias Stellar.XDR.{Int64, Void}
+  alias Stellar.XDR.Operations.AccountMergeResultCode
+
+  @behaviour XDR.Declaration
+
+  @arms [ACCOUNT_MERGE_SUCCESS: Int64, default: Void]
+
+  @type result :: Int64.t() | Void.t() | any()
+
+  @type t :: %__MODULE__{result: result(), code: AccountMergeResultCode.t()}
+
+  defstruct [:result, :code]
+
+  @spec new(result :: result(), code :: AccountMergeResultCode.t()) :: t()
+  def new(result, %AccountMergeResultCode{} = code),
+    do: %__MODULE__{result: result, code: code}
+
+  @impl true
+  def encode_xdr(%__MODULE__{result: result, code: code}) do
+    code
+    |> XDR.Union.new(@arms, result)
+    |> XDR.Union.encode_xdr()
+  end
+
+  @impl true
+  def encode_xdr!(%__MODULE__{result: result, code: code}) do
+    code
+    |> XDR.Union.new(@arms, result)
+    |> XDR.Union.encode_xdr!()
+  end
+
+  @impl true
+  def decode_xdr(bytes, spec \\ union_spec())
+
+  def decode_xdr(bytes, spec) do
+    case XDR.Union.decode_xdr(bytes, spec) do
+      {:ok, {{code, result}, rest}} -> {:ok, {new(result, code), rest}}
+      error -> error
+    end
+  end
+
+  @impl true
+  def decode_xdr!(bytes, spec \\ union_spec())
+
+  def decode_xdr!(bytes, spec) do
+    {{code, result}, rest} = XDR.Union.decode_xdr!(bytes, spec)
+    {new(result, code), rest}
+  end
+
+  @spec union_spec() :: XDR.Union.t()
+  defp union_spec do
+    nil
+    |> AccountMergeResultCode.new()
+    |> XDR.Union.new(@arms)
+  end
+end
---
+++ lib/xdr/operations/bump_sequence_result.ex
@@ -0,0 +1,58 @@
+defmodule Stellar.XDR.Operations.BumpSequenceResult do
+  @moduledoc """
+  Representation of Stellar `BumpSequenceResult` type.
+  """
+  alias Stellar.XDR.Void
+  alias Stellar.XDR.Operations.BumpSequenceResultCode
+
+  @behaviour XDR.Declaration
+
+  @arms [BUMP_SEQUENCE_SUCCESS: Void, default: Void]
+
+  @type t :: %__MODULE__{result: any(), code: BumpSequenceResultCode.t()}
+
+  defstruct [:result, :code]
+
+  @spec new(result :: any(), code :: BumpSequenceResultCode.t()) :: t()
+  def new(result, %BumpSequenceResultCode{} = code),
+    do: %__MODULE__{result: result, code: code}
+
+  @impl true
+  def encode_xdr(%__MODULE__{result: result, code: code}) do
+    code
+    |> XDR.Union.new(@arms, result)
+    |> XDR.Union.encode_xdr()
+  end
+
+  @impl true
+  def encode_xdr!(%__MODULE__{result: result, code: code}) do
+    code
+    |> XDR.Union.new(@arms, result)
+    |> XDR.Union.encode_xdr!()
+  end
+
+  @impl true
+  def decode_xdr(bytes, spec \\ union_spec())
+
+  def decode_xdr(bytes, spec) do
+    case XDR.Union.decode_xdr(bytes, spec) do
+      {:ok, {{code, result}, rest}} -> {:ok, {new(result, code), rest}}
+      error -> error
+    end
+  end
+
+  @impl true
+  def decode_xdr!(bytes, spec \\ union_spec())
+
+  def decode_xdr!(bytes, spec) do
+    {{code, result}, rest} = XDR.Union.decode_xdr!(bytes, spec)
+    {new(result, code), rest}
+  end
+
+  @spec union_spec() :: XDR.Union.t()
+  defp union_spec do
+    nil
+    |> BumpSequenceResultCode.new()
+    |> XDR.Union.new(@arms)
+  end
+end
