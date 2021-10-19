c0dce3b445bacb1d05eb588af1e8aadf56ea27f2
Juan Hurtado <juanmahurtado10@gmail.com>
2021-10-19 15:31:16 -0500

    Add XDR types for CreateClaimableBalance and ClaimClaimableBalance (#88)

---
+++ lib/xdr/operations/claim_claimable_balance_result_code.ex
@@ -0,0 +1,56 @@
+defmodule Stellar.XDR.Operations.ClaimClaimableBalanceResultCode do
+  @moduledoc """
+  Representation of Stellar `ClaimClaimableBalanceResultCode` type.
+  """
+  @behaviour XDR.Declaration
+
+  @declarations [
+    CLAIM_CLAIMABLE_BALANCE_SUCCESS: 0,
+    CLAIM_CLAIMABLE_BALANCE_DOES_NOT_EXIST: -1,
+    CLAIM_CLAIMABLE_BALANCE_CANNOT_CLAIM: -2,
+    CLAIM_CLAIMABLE_BALANCE_LINE_FULL: -3,
+    CLAIM_CLAIMABLE_BALANCE_NO_TRUST: -4,
+    CLAIM_CLAIMABLE_BALANCE_NOT_AUTHORIZED: -5
+  ]
+
+  @enum_spec %XDR.Enum{declarations: @declarations, identifier: nil}
+
+  @type t :: %__MODULE__{identifier: atom()}
+
+  defstruct [:identifier]
+
+  @spec new(code :: atom()) :: t()
+  def new(code), do: %__MODULE__{identifier: code}
+
+  @impl true
+  def encode_xdr(%__MODULE__{identifier: code}) do
+    @declarations
+    |> XDR.Enum.new(code)
+    |> XDR.Enum.encode_xdr()
+  end
+
+  @impl true
+  def encode_xdr!(%__MODULE__{identifier: code}) do
+    @declarations
+    |> XDR.Enum.new(code)
+    |> XDR.Enum.encode_xdr!()
+  end
+
+  @impl true
+  def decode_xdr(bytes, spec \\ @enum_spec)
+
+  def decode_xdr(bytes, spec) do
+    case XDR.Enum.decode_xdr(bytes, spec) do
+      {:ok, {%XDR.Enum{identifier: code}, rest}} -> {:ok, {new(code), rest}}
+      error -> error
+    end
+  end
+
+  @impl true
+  def decode_xdr!(bytes, spec \\ @enum_spec)
+
+  def decode_xdr!(bytes, spec) do
+    {%XDR.Enum{identifier: code}, rest} = XDR.Enum.decode_xdr!(bytes, spec)
+    {new(code), rest}
+  end
+end
---
+++ test/xdr/operations/create_claimable_balance_result_code_test.exs
@@ -0,0 +1,52 @@
+defmodule Stellar.XDR.Operations.CreateClaimableBalanceResultCodeTest do
+  use ExUnit.Case
+
+  alias Stellar.XDR.Operations.CreateClaimableBalanceResultCode
+
+  describe "CreateClaimableBalanceResultCode" do
+    setup do
+      %{
+        code: :CREATE_CLAIMABLE_BALANCE_SUCCESS,
+        result: CreateClaimableBalanceResultCode.new(:CREATE_CLAIMABLE_BALANCE_SUCCESS),
+        binary: <<0, 0, 0, 0>>
+      }
+    end
+
+    test "new/1", %{code: type} do
+      %CreateClaimableBalanceResultCode{identifier: ^type} =
+        CreateClaimableBalanceResultCode.new(type)
+    end
+
+    test "encode_xdr/1", %{result: result, binary: binary} do
+      {:ok, ^binary} = CreateClaimableBalanceResultCode.encode_xdr(result)
+    end
+
+    test "encode_xdr/1 with an invalid code" do
+      {:error, :invalid_key} =
+        CreateClaimableBalanceResultCode.encode_xdr(%CreateClaimableBalanceResultCode{
+          identifier: :TEST
+        })
+    end
+
+    test "encode_xdr!/1", %{result: result, binary: binary} do
+      ^binary = CreateClaimableBalanceResultCode.encode_xdr!(result)
+    end
+
+    test "decode_xdr/2", %{result: result, binary: binary} do
+      {:ok, {^result, ""}} = CreateClaimableBalanceResultCode.decode_xdr(binary)
+    end
+
+    test "decode_xdr/2 with an invalid declaration" do
+      {:error, :invalid_key} = CreateClaimableBalanceResultCode.decode_xdr(<<1, 0, 0, 1>>)
+    end
+
+    test "decode_xdr!/2", %{result: result, binary: binary} do
+      {^result, ^binary} = CreateClaimableBalanceResultCode.decode_xdr!(binary <> binary)
+    end
+
+    test "decode_xdr!/2 with an error code" do
+      {%CreateClaimableBalanceResultCode{identifier: :CREATE_CLAIMABLE_BALANCE_LOW_RESERVE}, ""} =
+        CreateClaimableBalanceResultCode.decode_xdr!(<<255, 255, 255, 254>>)
+    end
+  end
+end
