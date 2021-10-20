cae636188c0607d2ba0cc0e9a4204de67eb37ce8
Juan Hurtado <juan@kommit.co>
2021-10-20 15:13:05 -0500

    Add XDR types for Clawback and LiquidityPool operation results

---
+++ lib/xdr/operations/liquidity_pool_deposit_result.ex
@@ -0,0 +1,58 @@
+defmodule Stellar.XDR.Operations.LiquidityPoolDepositResult do
+  @moduledoc """
+  Representation of Stellar `LiquidityPoolDepositResult` type.
+  """
+  alias Stellar.XDR.Void
+  alias Stellar.XDR.Operations.LiquidityPoolDepositResultCode
+
+  @behaviour XDR.Declaration
+
+  @arms [LIQUIDITY_POOL_DEPOSIT_SUCCESS: Void, default: Void]
+
+  @type t :: %__MODULE__{result: any(), code: LiquidityPoolDepositResultCode.t()}
+
+  defstruct [:result, :code]
+
+  @spec new(result :: any(), code :: LiquidityPoolDepositResultCode.t()) :: t()
+  def new(result, %LiquidityPoolDepositResultCode{} = code),
+    do: %__MODULE__{result: result, code: code}
+
+  @impl true
+  def encode_xdr(%__MODULE__{result: result, code: code}) do
+    code
+    |> XDR.Union.new(@arms, result)
+    |> XDR.Union.encode_xdr()
+  end
+
+  @impl true
+  def encode_xdr!(%__MODULE__{result: result, code: code}) do
+    code
+    |> XDR.Union.new(@arms, result)
+    |> XDR.Union.encode_xdr!()
+  end
+
+  @impl true
+  def decode_xdr(bytes, spec \\ union_spec())
+
+  def decode_xdr(bytes, spec) do
+    case XDR.Union.decode_xdr(bytes, spec) do
+      {:ok, {{code, result}, rest}} -> {:ok, {new(result, code), rest}}
+      error -> error
+    end
+  end
+
+  @impl true
+  def decode_xdr!(bytes, spec \\ union_spec())
+
+  def decode_xdr!(bytes, spec) do
+    {{code, result}, rest} = XDR.Union.decode_xdr!(bytes, spec)
+    {new(result, code), rest}
+  end
+
+  @spec union_spec() :: XDR.Union.t()
+  defp union_spec do
+    nil
+    |> LiquidityPoolDepositResultCode.new()
+    |> XDR.Union.new(@arms)
+  end
+end
---
+++ lib/xdr/operations/liquidity_pool_deposit_result_code.ex
@@ -0,0 +1,66 @@
+defmodule Stellar.XDR.Operations.LiquidityPoolDepositResultCode do
+  @moduledoc """
+  Representation of Stellar `LiquidityPoolDepositResultCode` type.
+  """
+  @behaviour XDR.Declaration
+
+  @declarations [
+    # success
+    LIQUIDITY_POOL_DEPOSIT_SUCCESS: 0,
+    # bad input
+    LIQUIDITY_POOL_DEPOSIT_MALFORMED: -1,
+    # no trust line for one of the assets
+    LIQUIDITY_POOL_DEPOSIT_NO_TRUST: -2,
+    # not authorized for one of the assets
+    LIQUIDITY_POOL_DEPOSIT_NOT_AUTHORIZED: -3,
+    # not enough balance for one of the assets
+    LIQUIDITY_POOL_DEPOSIT_UNDERFUNDED: -4,
+    # pool share trust line doesn't have sufficient limit
+    LIQUIDITY_POOL_DEPOSIT_LINE_FULL: -5,
+    # deposit price outside bounds
+    LIQUIDITY_POOL_DEPOSIT_BAD_PRICE: -6,
+    # pool reserves are full
+    LIQUIDITY_POOL_DEPOSIT_POOL_FULL: -7
+  ]
+
+  @enum_spec %XDR.Enum{declarations: @declarations, identifier: nil}
+
+  @type t :: %__MODULE__{identifier: atom()}
+
+  defstruct [:identifier]
+
+  @spec new(code :: atom()) :: t()
+  def new(code), do: %__MODULE__{identifier: code}
+
+  @impl true
+  def encode_xdr(%__MODULE__{identifier: code}) do
+    @declarations
+    |> XDR.Enum.new(code)
+    |> XDR.Enum.encode_xdr()
+  end
+
+  @impl true
+  def encode_xdr!(%__MODULE__{identifier: code}) do
+    @declarations
+    |> XDR.Enum.new(code)
+    |> XDR.Enum.encode_xdr!()
+  end
+
+  @impl true
+  def decode_xdr(bytes, spec \\ @enum_spec)
+
+  def decode_xdr(bytes, spec) do
+    case XDR.Enum.decode_xdr(bytes, spec) do
+      {:ok, {%XDR.Enum{identifier: code}, rest}} -> {:ok, {new(code), rest}}
+      error -> error
+    end
+  end
+
+  @impl true
+  def decode_xdr!(bytes, spec \\ @enum_spec)
+
+  def decode_xdr!(bytes, spec) do
+    {%XDR.Enum{identifier: code}, rest} = XDR.Enum.decode_xdr!(bytes, spec)
+    {new(code), rest}
+  end
+end
