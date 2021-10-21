925a04d2b3408d1d22c4abc2341da0acceed9315
Juan Hurtado <juan@kommit.co>
2021-10-21 18:34:53 -0500

    Add XDR types for TransactionResults

---
+++ lib/xdr/transactions/transaction_result.ex
@@ -0,0 +1,56 @@
+defmodule Stellar.XDR.TransactionResult do
+  @moduledoc """
+  Representation of Stellar `TransactionResult` type.
+  """
+  alias Stellar.XDR.{Int64, TxResult}
+  alias Stellar.XDR.TransactionExt, as: Ext
+
+  @behaviour XDR.Declaration
+
+  @struct_spec XDR.Struct.new(fee_charged: Int64, result: TxResult, ext: Ext)
+
+  @type t :: %__MODULE__{fee_charged: Int64.t(), result: TxResult.t(), ext: Ext.t()}
+
+  defstruct [:fee_charged, :result, :ext]
+
+  @spec new(fee_charged :: Int64.t(), result :: TxResult.t(), ext :: Ext.t()) :: t()
+  def new(%Int64{} = fee_charged, %TxResult{} = result, %Ext{} = ext),
+    do: %__MODULE__{fee_charged: fee_charged, result: result, ext: ext}
+
+  @impl true
+  def encode_xdr(%__MODULE__{fee_charged: fee_charged, result: result, ext: ext}) do
+    [fee_charged: fee_charged, result: result, ext: ext]
+    |> XDR.Struct.new()
+    |> XDR.Struct.encode_xdr()
+  end
+
+  @impl true
+  def encode_xdr!(%__MODULE__{fee_charged: fee_charged, result: result, ext: ext}) do
+    [fee_charged: fee_charged, result: result, ext: ext]
+    |> XDR.Struct.new()
+    |> XDR.Struct.encode_xdr!()
+  end
+
+  @impl true
+  def decode_xdr(bytes, struct \\ @struct_spec)
+
+  def decode_xdr(bytes, struct) do
+    case XDR.Struct.decode_xdr(bytes, struct) do
+      {:ok, {%XDR.Struct{components: [fee_charged: fee_charged, result: result, ext: ext]}, rest}} ->
+        {:ok, {new(fee_charged, result, ext), rest}}
+
+      error ->
+        error
+    end
+  end
+
+  @impl true
+  def decode_xdr!(bytes, struct \\ @struct_spec)
+
+  def decode_xdr!(bytes, struct) do
+    {%XDR.Struct{components: [fee_charged: fee_charged, result: result, ext: ext]}, rest} =
+      XDR.Struct.decode_xdr!(bytes, struct)
+
+    {new(fee_charged, result, ext), rest}
+  end
+end
