e4e37d205440acd8bae344877d4345ae5fb9675c
Juan Hurtado <juanmahurtado10@gmail.com>
2021-12-03 09:46:47 -0500

    Prepare v0.2.2 release (#115)

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule StellarBase.MixProject do
   use Mix.Project
 
   @github_url "https://github.com/kommitters/stellar_base"
-  @version "0.2.1"
+  @version "0.2.2"
 
   def project do
     [
