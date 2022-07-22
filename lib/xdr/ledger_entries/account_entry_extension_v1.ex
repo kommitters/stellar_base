be079e45f1a3e200f7117b249038ace0b85c02d5
CristhianRodriguezMolina <camilo.r13@hotmail.com>
2022-07-22 15:32:43 -0500

    Added extension to account_entry_ext_v1 and added test

--- lib/xdr/ledger_entries/account_entry_extension_v1.ex
+++ lib/xdr/ledger_entries/account_entry_extension_v1.ex
@@ -3,32 +3,51 @@ defmodule StellarBase.XDR.AccountEntryExtensionV1 do
   Representation of Stellar's ledger AccountEntryExtensionV1
   """
 
-  alias StellarBase.XDR.{Liabilities}
+  alias StellarBase.XDR.{Liabilities, AccountEntryExtensionV1Ext}
 
   @behaviour XDR.Declaration
 
-  @struct_spec XDR.Struct.new(liabilities: Liabilities)
+  @struct_spec XDR.Struct.new(
+                 liabilities: Liabilities,
+                 account_entry_extension_v1_ext: AccountEntryExtensionV1Ext
+               )
 
   @type t :: %__MODULE__{
-          liabilities: Liabilities.t()
+          liabilities: Liabilities.t(),
+          account_entry_extension_v1_ext: AccountEntryExtensionV1Ext.t()
         }
 
-  defstruct [:liabilities]
+  defstruct [:liabilities, :account_entry_extension_v1_ext]
 
-  @spec new(liabilities :: Liabilities.t()) :: t()
-  def new(%Liabilities{} = liabilities),
-    do: %__MODULE__{liabilities: liabilities}
+  @spec new(
+          liabilities :: Liabilities.t(),
+          account_entry_extension_v1_ext :: AccountEntryExtensionV1Ext.t()
+        ) :: t()
+  def new(
+        %Liabilities{} = liabilities,
+        %AccountEntryExtensionV1Ext{} = account_entry_extension_v1_ext
+      ),
+      do: %__MODULE__{
+        liabilities: liabilities,
+        account_entry_extension_v1_ext: account_entry_extension_v1_ext
+      }
 
   @impl true
-  def encode_xdr(%__MODULE__{liabilities: liabilities}) do
-    [liabilities: liabilities]
+  def encode_xdr(%__MODULE__{
+        liabilities: liabilities,
+        account_entry_extension_v1_ext: account_entry_extension_v1_ext
+      }) do
+    [liabilities: liabilities, account_entry_extension_v1_ext: account_entry_extension_v1_ext]
     |> XDR.Struct.new()
     |> XDR.Struct.encode_xdr()
   end
 
   @impl true
-  def encode_xdr!(%__MODULE__{liabilities: liabilities}) do
-    [liabilities: liabilities]
+  def encode_xdr!(%__MODULE__{
+        liabilities: liabilities,
+        account_entry_extension_v1_ext: account_entry_extension_v1_ext
+      }) do
+    [liabilities: liabilities, account_entry_extension_v1_ext: account_entry_extension_v1_ext]
     |> XDR.Struct.new()
     |> XDR.Struct.encode_xdr!()
   end
@@ -40,9 +59,12 @@ defmodule StellarBase.XDR.AccountEntryExtensionV1 do
     case XDR.Struct.decode_xdr(bytes, struct) do
       {:ok,
        {%XDR.Struct{
-          components: [liabilities: liabilities]
+          components: [
+            liabilities: liabilities,
+            account_entry_extension_v1_ext: account_entry_extension_v1_ext
+          ]
         }, rest}} ->
-        {:ok, {new(liabilities), rest}}
+        {:ok, {new(liabilities, account_entry_extension_v1_ext), rest}}
 
       error ->
         error
@@ -54,9 +76,12 @@ defmodule StellarBase.XDR.AccountEntryExtensionV1 do
 
   def decode_xdr!(bytes, struct) do
     {%XDR.Struct{
-       components: [liabilities: liabilities]
+       components: [
+         liabilities: liabilities,
+         account_entry_extension_v1_ext: account_entry_extension_v1_ext
+       ]
      }, rest} = XDR.Struct.decode_xdr!(bytes, struct)
 
-    {new(liabilities), rest}
+    {new(liabilities, account_entry_extension_v1_ext), rest}
   end
 end
