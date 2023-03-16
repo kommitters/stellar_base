d1a53665e3e1808128018f80bf1129461e74a5c1
CristhianRodriguezMolina <50219561+CristhianRodriguezMolina@users.noreply.github.com>
2023-03-16 15:31:15 -0500

    sc spec udt enum and error structs

---
+++ lib/xdr/contract/spec/sc_spec_udt_enum_v0.ex
@@ -0,0 +1,121 @@
+defmodule StellarBase.XDR.SCSpecUDTEnumV0 do
+  @moduledoc """
+  Representation of Stellar's ledger SCSpecUDTEnumV0
+  """
+
+  alias StellarBase.XDR.{String1024, String80, String60, SCSpecUDTEnumCaseV0List}
+
+  @behaviour XDR.Declaration
+
+  @struct_spec XDR.Struct.new(
+                 doc: String1024,
+                 lib: String80,
+                 name: String60,
+                 cases: SCSpecUDTEnumCaseV0List
+               )
+
+  @type doc :: String1024.t()
+  @type lib :: String80.t()
+  @type name :: String60.t()
+  @type cases :: SCSpecUDTEnumCaseV0List.t()
+
+  @type t :: %__MODULE__{doc: doc(), lib: lib(), name: name(), cases: cases()}
+
+  defstruct [:doc, :lib, :name, :cases]
+
+  @spec new(
+          doc :: String1024.t(),
+          lib :: String80.t(),
+          name :: String60.t(),
+          cases :: SCSpecUDTEnumCaseV0List.t()
+        ) :: t()
+  def new(
+        %String1024{} = doc,
+        %String80{} = lib,
+        %String60{} = name,
+        %SCSpecUDTEnumCaseV0List{} = cases
+      ),
+      do: %__MODULE__{
+        doc: doc,
+        lib: lib,
+        name: name,
+        cases: cases
+      }
+
+  @impl true
+  def encode_xdr(%__MODULE__{
+        doc: doc,
+        lib: lib,
+        name: name,
+        cases: cases
+      }) do
+    [
+      doc: doc,
+      lib: lib,
+      name: name,
+      cases: cases
+    ]
+    |> XDR.Struct.new()
+    |> XDR.Struct.encode_xdr()
+  end
+
+  @impl true
+  def encode_xdr!(%__MODULE__{
+        doc: doc,
+        lib: lib,
+        name: name,
+        cases: cases
+      }) do
+    [
+      doc: doc,
+      lib: lib,
+      name: name,
+      cases: cases
+    ]
+    |> XDR.Struct.new()
+    |> XDR.Struct.encode_xdr!()
+  end
+
+  @impl true
+  def decode_xdr(bytes, struct \\ @struct_spec)
+
+  def decode_xdr(bytes, struct) do
+    case XDR.Struct.decode_xdr(bytes, struct) do
+      {:ok,
+       {%XDR.Struct{
+          components: [
+            doc: doc,
+            lib: lib,
+            name: name,
+            cases: cases
+          ]
+        }, rest}} ->
+        {:ok,
+         {new(
+            doc,
+            lib,
+            name,
+            cases
+          ), rest}}
+
+      error ->
+        error
+    end
+  end
+
+  @impl true
+  def decode_xdr!(bytes, struct \\ @struct_spec)
+
+  def decode_xdr!(bytes, struct) do
+    {%XDR.Struct{
+       components: [
+         doc: doc,
+         lib: lib,
+         name: name,
+         cases: cases
+       ]
+     }, rest} = XDR.Struct.decode_xdr!(bytes, struct)
+
+    {new(doc, lib, name, cases), rest}
+  end
+end
---
+++ test/xdr/contract/spec/sc_spec_udt_error_enum_v0_test.exs
@@ -0,0 +1,75 @@
+defmodule StellarBase.XDR.SCSpecUDTErrorEnumV0Test do
+  use ExUnit.Case
+
+  alias StellarBase.XDR.{
+    UInt32,
+    SCSpecUDTErrorEnumV0,
+    SCSpecUDTErrorEnumCaseV0,
+    SCSpecUDTErrorEnumCaseV0List,
+    String60,
+    String80,
+    String1024
+  }
+
+  describe "SCSpecUDTErrorEnumCaseV0" do
+    setup do
+      doc = String1024.new("Hello there")
+      lib = String80.new("Hello there")
+      name = String60.new("Hello there")
+      value = UInt32.new(4_294_967_295)
+      sc_spec_udt_error_enum_case_v0 = [SCSpecUDTErrorEnumCaseV0.new(doc, name, value)]
+      cases = SCSpecUDTErrorEnumCaseV0List.new(sc_spec_udt_error_enum_case_v0)
+
+      %{
+        doc: doc,
+        lib: lib,
+        name: name,
+        cases: cases,
+        sc_spec_udt_error_enum_v0: SCSpecUDTErrorEnumV0.new(doc, lib, name, cases),
+        binary:
+          <<0, 0, 0, 11, 72, 101, 108, 108, 111, 32, 116, 104, 101, 114, 101, 0, 0, 0, 0, 11, 72,
+            101, 108, 108, 111, 32, 116, 104, 101, 114, 101, 0, 0, 0, 0, 11, 72, 101, 108, 108,
+            111, 32, 116, 104, 101, 114, 101, 0, 0, 0, 0, 1, 0, 0, 0, 11, 72, 101, 108, 108, 111,
+            32, 116, 104, 101, 114, 101, 0, 0, 0, 0, 11, 72, 101, 108, 108, 111, 32, 116, 104,
+            101, 114, 101, 0, 255, 255, 255, 255>>
+      }
+    end
+
+    test "new/1", %{doc: doc, lib: lib, name: name, cases: cases} do
+      %SCSpecUDTErrorEnumV0{doc: ^doc, lib: ^lib, name: ^name, cases: ^cases} =
+        SCSpecUDTErrorEnumV0.new(doc, lib, name, cases)
+    end
+
+    test "encode_xdr/1", %{
+      sc_spec_udt_error_enum_v0: sc_spec_udt_error_enum_v0,
+      binary: binary
+    } do
+      {:ok, ^binary} = SCSpecUDTErrorEnumV0.encode_xdr(sc_spec_udt_error_enum_v0)
+    end
+
+    test "encode_xdr!/1", %{
+      sc_spec_udt_error_enum_v0: sc_spec_udt_error_enum_v0,
+      binary: binary
+    } do
+      ^binary = SCSpecUDTErrorEnumV0.encode_xdr!(sc_spec_udt_error_enum_v0)
+    end
+
+    test "decode_xdr/2", %{
+      sc_spec_udt_error_enum_v0: sc_spec_udt_error_enum_v0,
+      binary: binary
+    } do
+      {:ok, {^sc_spec_udt_error_enum_v0, ""}} = SCSpecUDTErrorEnumV0.decode_xdr(binary)
+    end
+
+    test "decode_xdr/2 with an invalid binary" do
+      {:error, :not_binary} = SCSpecUDTErrorEnumV0.decode_xdr(123)
+    end
+
+    test "decode_xdr!/2", %{
+      sc_spec_udt_error_enum_v0: sc_spec_udt_error_enum_v0,
+      binary: binary
+    } do
+      {^sc_spec_udt_error_enum_v0, ^binary} = SCSpecUDTErrorEnumV0.decode_xdr!(binary <> binary)
+    end
+  end
+end
